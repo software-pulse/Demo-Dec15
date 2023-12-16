605af17243c6c67def24748a995012e34f163783
software <38241539+software-pulse@users.noreply.github.com>
2023-12-16 15:21:52 +0530

    Update rcm-SIPml-api.js

--- rcm-SIPml-api.js
+++ rcm-SIPml-api.js
@@ -8,7 +8,6 @@
 
 // ================== DEBUG CODE =============== //
 var __b_release_mode = false;
-var __b_release_mode = false;
 var tag_hdr = document.getElementsByTagName('head')[0];
 ['SIPml.js', 'src/tinySIP/src/tsip_api.js'].forEach(function (file) {
     var tag_script = document.createElement('script');
@@ -16,7 +15,10 @@ var tag_hdr = document.getElementsByTagName('head')[0];
     tag_script.setAttribute('src', file + "?svn=252");
     tag_hdr.appendChild(tag_script);
 });
-window.console &&window.console.warn && window.console.warn("YOUR ARE USING DEBUG CODE. PLEASE USE CODE UNDER 'release' FOLDER or check https://code.google.com/p/sipml5/wiki/FAQ#How_to_reduce_the_size_of_the_scripts_before_deploying for more information on how to amalgamate the code.");
+window.console &&window.console.warn && window.console.warn("YOUR ARE USING DEBUG CODE.
+PLEASE USE CODE UNDER 'release' FOLDER or check https://code.google.com/p/sipml5/wiki/
+FAQ#How_to_reduce_the_size_of_the_scripts_before_deploying for more information on how
+to amalgamate the code.");
 
 
 // ================== RELEASE CODE =============== //
